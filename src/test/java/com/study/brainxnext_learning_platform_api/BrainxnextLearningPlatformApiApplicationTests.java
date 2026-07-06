d567d4eac723e17691bccb43d88bcacee55c0ae9
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2026-07-06 23:20:02 +0600

    first commit

---
+++ src/test/java/com/study/brainxnext_learning_platform_api/BrainxnextLearningPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.brainxnext_learning_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainxnextLearningPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
